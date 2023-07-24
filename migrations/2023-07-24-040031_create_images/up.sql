b6252c2eb3a659c04be7598594fd5abbae4c373b
tdc <tdc@tdccc.com.tw>
2023-07-24 12:01:11 +0800

    ⚡️ diesel chrono::Native

---
+++ migrations/2023-07-24-040031_create_images/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+CREATE TABLE images (
+  id UUID PRIMARY KEY,
+  date TIME DEFAULT NULL,
+  upload_by VARCHAR DEFAULT NULL,
+  md5 VARCHAR DEFAULT NULL,
+  origin_filename VARCHAR DEFAULT NULL
+)
--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,4 @@
-// use std::time::SystemTime;
-use chrono::DateTime;
 use chrono::naive::NaiveDateTime;
-use chrono::prelude::*;
 use diesel::prelude::*;
 use uuid::Uuid;
 use serde::{Deserialize, Serialize};
@@ -19,7 +16,7 @@ pub struct Users {
 #[diesel(table_name = crate::schema::images)]
 pub struct Images {
     pub id: Uuid,         // auto increment
-    pub date: DateTime<Utc>,
+    pub date: NaiveDateTime,
     pub upload_by: i32,   // relate to user id
     pub md5: String,      // file md5 hash
     pub origin_filename: String,
